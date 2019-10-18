8ce877f2024602518a5ae5aa3e84a1f401973879
Sam Bernal <sambernal@konnichiwa-sams-computer-desu.local>
2019-10-18 13:43:57 -0400

    able to write on DOM with giphy API data

---
+++ infinite_kitties/js/app.js
@@ -0,0 +1,49 @@
+//testing giphy API
+
+
+
+$(() => {
+
+    const $gif = $('<iframe>').attr('frameBorder', '0');
+
+    $.ajax({
+        url:'https://api.giphy.com/v1/gifs/search?api_key=xj0u5sGZ4wDzUWFeai6BjNWSAuOIlmUk&q=cats&limit=1&offset=0&rating=PG&lang=en'
+        }).then(
+        (data)=>{
+
+            // console.log(data.data);
+            // console.log(data.data[0]);
+            // console.log(data.data[0].images);
+            // console.log(data.data[0].images.original);
+
+
+            //get url for the src on iframe
+            // console.log(data.data[0].embed_url);
+            const $url = data.data[0].embed_url;
+
+            //height and width for the iframe attr
+            // console.log(data.data[0].images.original.height);
+            const $height = data.data[0].images.original.height;
+
+            // console.log(data.data[0].images.original.width);
+            const $width = data.data[0].images.original.width;
+
+            //adding to the $gif variable
+
+            $gif.attr('src', $url);
+            $gif.attr('height', $height)
+            $gif.attr('width', $width)
+
+            //appending to the body
+            $('body').append($gif)
+
+
+
+        },
+        ()=>{
+            console.log('bad request');
+        }
+    );
+
+
+})
