e338c071a7b9224e6466518a832d61ad0494afab
Sam Bernal <sambernal@konnichiwa-sams-computer-desu.local>
2019-10-19 10:36:39 -0400

    able to search for doggos too

--- infinite_kitties/js/app.js
+++ infinite_kitties/js/app.js
@@ -3,48 +3,54 @@
 
 
 $(() => {
-
     const $gif = $('<iframe>').attr('frameBorder', '0');
-    const randomIndex = Math.floor(Math.random() * 100);
+    const $container = $('.container')
 
-    $.ajax({
-        url:'https://api.giphy.com/v1/gifs/search?api_key=xj0u5sGZ4wDzUWFeai6BjNWSAuOIlmUk&q=cats&limit=1&offset='+randomIndex+'&rating=PG&lang=en'
-        }).then(
-        (data)=>{
+    $('.gifButton').on('click', (event) => {
+        const randomIndex = Math.floor(Math.random() * 1000);
+        const search = $(event.target).attr('id');
 
-            // console.log(data.data);
-            // console.log(data.data[0]);
-            // console.log(data.data[0].images);
-            // console.log(data.data[0].images.original);
+        $.ajax({
+            url:'https://api.giphy.com/v1/gifs/search?api_key=xj0u5sGZ4wDzUWFeai6BjNWSAuOIlmUk&q='+search+'&limit=5&offset='+randomIndex+'&rating=PG&lang=en'
+            }).then(
+            (data)=>{
+
+                // console.log(data.data);
+                // console.log(data.data[0]);
+                // console.log(data.data[0].images);
+                // console.log(data.data[0].images.original);
+
+                //get url for the src on iframe
+                console.log(data.data[0].embed_url);
+                const $url = data.data[0].embed_url;
+
+                //height and width for the iframe attr
+                // console.log(data.data[0].images.original.height);
+                const $height = data.data[0].images.original.height;
+
+                // console.log(data.data[0].images.original.width);
+                const $width = data.data[0].images.original.width;
+
+                //adding to the $gif variable
+
+                $gif.attr('src', $url);
+                $gif.attr('height', $height)
+                $gif.attr('width', $width)
+
+                //appending to the body
+                $container.append($gif)
 
 
-            //get url for the src on iframe
-            console.log(data.data[0].embed_url);
-            const $url = data.data[0].embed_url;
 
-            //height and width for the iframe attr
-            // console.log(data.data[0].images.original.height);
-            const $height = data.data[0].images.original.height;
+            },
+            ()=>{
+                console.log('bad request');
+            }
+        );
 
-            // console.log(data.data[0].images.original.width);
-            const $width = data.data[0].images.original.width;
-
-            //adding to the $gif variable
-
-            $gif.attr('src', $url);
-            $gif.attr('height', $height)
-            $gif.attr('width', $width)
-
-            //appending to the body
-            $('body').append($gif)
+    }) //end of on click handler
 
 
-
-        },
-        ()=>{
-            console.log('bad request');
-        }
-    );
 
 
 })
