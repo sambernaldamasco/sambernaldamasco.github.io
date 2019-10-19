5266d7e18bd0d1ef9060b1f9a6cdaf28bdc1a9bc
Sam Bernal <sambernal@konnichiwa-sams-computer-desu.local>
2019-10-19 11:01:29 -0400

    able to hide all gifs and only show the first one

--- infinite_kitties/js/app.js
+++ infinite_kitties/js/app.js
@@ -3,10 +3,11 @@
 
 
 $(() => {
-    const $gif = $('<iframe>').attr('frameBorder', '0');
-    const $container = $('.container')
+
+    const $gifsContainer = $('.carousel-gifs')
 
     $('.gifButton').on('click', (event) => {
+        $gifsContainer.empty();
         const randomIndex = Math.floor(Math.random() * 1000);
         const search = $(event.target).attr('id');
 
@@ -19,26 +20,47 @@ $(() => {
                 // console.log(data.data[0]);
                 // console.log(data.data[0].images);
                 // console.log(data.data[0].images.original);
+                for(let i = 0; i < 5; i++){
+                    const $gif = $('<iframe>').attr('frameBorder', '0');
+                    console.log(data.data[i].embed_url);
+                    const $url = data.data[i].embed_url;
 
+                    //height and width for the iframe attr
+                    // console.log(data.data[i].images.original.height);
+                    const $height = data.data[i].images.original.height;
+
+                    // console.log(data.data[i].images.original.width);
+                    const $width = data.data[i].images.original.width;
+
+                    //adding to the $gif variable
+
+                    $gif.attr('src', $url);
+                    $gif.attr('height', $height);
+                    $gif.attr('width', $width);
+
+                    console.log($gif);
+                    //appending to the body
+                    $gifsContainer.append($gif);
+                }
                 //get url for the src on iframe
-                console.log(data.data[0].embed_url);
-                const $url = data.data[0].embed_url;
-
-                //height and width for the iframe attr
-                // console.log(data.data[0].images.original.height);
-                const $height = data.data[0].images.original.height;
-
-                // console.log(data.data[0].images.original.width);
-                const $width = data.data[0].images.original.width;
-
-                //adding to the $gif variable
-
-                $gif.attr('src', $url);
-                $gif.attr('height', $height)
-                $gif.attr('width', $width)
-
-                //appending to the body
-                $container.append($gif)
+                // console.log(data.data[0].embed_url);
+                // const $url = data.data[0].embed_url;
+                //
+                // //height and width for the iframe attr
+                // // console.log(data.data[0].images.original.height);
+                // const $height = data.data[0].images.original.height;
+                //
+                // // console.log(data.data[0].images.original.width);
+                // const $width = data.data[0].images.original.width;
+                //
+                // //adding to the $gif variable
+                //
+                // $gif.attr('src', $url);
+                // $gif.attr('height', $height)
+                // $gif.attr('width', $width)
+                //
+                // //appending to the body
+                // $container.append($gif)
 
 
 
