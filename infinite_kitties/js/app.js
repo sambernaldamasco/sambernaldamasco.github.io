f01cb68604e1be9f6ca4aad5d6420ce41c9b40d5
Sam Bernal <sambernal@konnichiwa-sams-computer-desu.local>
2019-10-20 13:29:55 -0400

    made the app.js DRYer

--- infinite_kitties/js/app.js
+++ infinite_kitties/js/app.js
@@ -1,87 +1,128 @@
+//////////////////////////////////////////
+// TO DO                                //
+//////////////////////////////////////////
+
 //testing giphy API
 
+//trying to make the code DRY
+
+//trying to create objects for respective sections
 
 
 $(() => {
+
+    //Global variables
     const $gifsContainer = $('.carousel-gifs')
     let currentIndex = 0;
     let highestIndex = 0;
 
 
-    $('.gifButton').on('click', (event) => {
+    //trying to make the code DRY
+    const UI = {
+        //defining addGif method
+        //params: data(object) -- comes from AJAX call
+        //returns nothing
+        //summary: for each data object coming from the AJAX call, it loops 50 times(the amount of limit of the API). Grabs the embed URL, height and width, then appends an iframe element to the gifs container. It also changes the value of the variable highestIndex based on the amount of elements added.
+        addGif: (data) => {
+            for(let i = 0; i < 50; i++){
+                const $gif = $('<iframe>').attr('frameBorder', '0');
 
-        const randomIndex = Math.floor(Math.random() * 1000);
-        const search = $(event.target).attr('id');
+                const $url = data.data[i].embed_url;
 
-        $('.carousel-button').css('display', 'block');
+                //height and width for the iframe attr
+                const $height = data.data[i].images.original.height;
 
-        $gifsContainer.empty();
+                const $width = data.data[i].images.original.width;
 
-        $.ajax({
-            url:'https://api.giphy.com/v1/gifs/search?api_key=xj0u5sGZ4wDzUWFeai6BjNWSAuOIlmUk&q='+search+'&limit=100&offset='+randomIndex+'&rating=PG&lang=en'
-            }).then(
-            (data)=>{
+                //adding to the $gif variable
+                $gif.attr('src', $url);
+                $gif.attr('height', $height);
+                $gif.attr('width', $width);
 
-                // console.log(data.data);
-                // console.log(data.data[0]);
-                // console.log(data.data[0].images);
-                // console.log(data.data[0].images.original);
-                for(let i = 0; i < 50; i++){
-                    const $gif = $('<iframe>').attr('frameBorder', '0');
-                    console.log(data.data[i].embed_url);
-                    const $url = data.data[i].embed_url;
+                //appending to the body
+                $gifsContainer.append($gif);
+            }
+            highestIndex = $('.carousel-gifs').children().length -1;
+        }
+    } //end of UI object
 
-                    //height and width for the iframe attr
-                    // console.log(data.data[i].images.original.height);
-                    const $height = data.data[i].images.original.height;
 
-                    // console.log(data.data[i].images.original.width);
-                    const $width = data.data[i].images.original.width;
+    const App = {
+        //defining ajaxCall
+        //params: search(string), randomIndex(int)
+        //returns nothing
+        //summary: calls the giphy API to return data based on the search query(search variable) and the index for the search(offset), if the call is successfull, it calls the method addGif passing data as an argument
+        ajaxCall: (search, randomIndex) => {
+            $.ajax({
+                url:'https://api.giphy.com/v1/gifs/search?api_key=xj0u5sGZ4wDzUWFeai6BjNWSAuOIlmUk&q='+search+'&limit=100&offset='+randomIndex+'&rating=PG&lang=en'
+                }).then(
+                (data)=>{
+                    UI.addGif(data);
+                },
+                ()=>{
+                    console.log('bad request');
+                })
+        }
+    } //end of App object
 
-                    //adding to the $gif variable
 
-                    $gif.attr('src', $url);
-                    $gif.attr('height', $height);
-                    $gif.attr('width', $width);
+    const EventHandlers = {
+        //defining generateGif
+        //params: event
+        //returns nothing
+        //summary: it generates a random index amount and based on the button clicked(dogs or cats), it defines the sarch query for the ajax call. It also empties the gifs container.
+        generateGif: (event) => {
+            const randomIndex = Math.floor(Math.random() * 1000);
+            const search = $(event.target).attr('id');
 
-                    console.log($gif);
-                    //appending to the body
-                    $gifsContainer.append($gif);
-                }
-                highestIndex = $('.carousel-gifs').children().length -1;
-            },
-            ()=>{
-                console.log('bad request');
+            $('.carousel-button').css('display', 'block');
+
+            $gifsContainer.empty();
+
+            App.ajaxCall(search, randomIndex);
+        },
+
+        //defining nextGif
+        //params none
+        //returns nothing
+        //summary: when clicked, changes the display of current gif appended to none, and it goes to the next gif appended and changes the display to block, based on the current index of the children appended on the carousel-gifs container.
+        nextGif: () => {
+            $('.carousel-gifs').children().eq(currentIndex).css('display', 'none');
+
+            if (currentIndex < highestIndex){
+                currentIndex++
+            } else {
+                currentIndex = 0;
             }
 
-        );//end of then method
-    }) //end of on click handler
+            $('.carousel-gifs').children().eq(currentIndex).css('display', 'block');
+        },
 
-    $('.next').on('click', () => {
+        //defining previousGif
+        //params none
+        //returns nothing
+        //summary: when clicked, changes the display of current gif appended to none, and it goes to the previous gif appended and changes the display to block, based on the current index of the children appended on the carousel-gifs container.
+        previousGif: () => {
+            $('.carousel-gifs').children().eq(currentIndex).css('display', 'none');
 
-        $('.carousel-gifs').children().eq(currentIndex).css('display', 'none');
+            if (currentIndex > 0){
+                currentIndex--
+            } else {
+                currentIndex = highestIndex;
+            }
 
-        if (currentIndex < highestIndex){
-            currentIndex++
-        } else {
-            currentIndex = 0;
+            $('.carousel-gifs').children().eq(currentIndex).css('display', 'block');
         }
+    }
 
-        $('.carousel-gifs').children().eq(currentIndex).css('display', 'block');
-    })
 
-    $('.previous').on('click', () => {
 
-        $('.carousel-gifs').children().eq(currentIndex).css('display', 'none');
+    //event listeners
+    $('.gifButton').on('click', EventHandlers.generateGif);
 
-        if (currentIndex > 0){
-            currentIndex--
-        } else {
-            currentIndex = highestIndex;
-        }
+    $('.next').on('click', EventHandlers.nextGif);
 
-        $('.carousel-gifs').children().eq(currentIndex).css('display', 'block');
-    })
+    $('.previous').on('click', EventHandlers.previousGif);
 
 
 })
